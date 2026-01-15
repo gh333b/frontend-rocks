f382da788c2112770ee3ac08af96705f6aa7982a
gh333b <151088778+gh333b@users.noreply.github.com>
2026-01-15 13:34:24 +0000

    prima versione

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
 
-export const Root = () => {return <div className="space-x-2 space-y-2 flex flex-wrap text-l ">
+export const Root = () => {return <div className="space-x-2 space-y-2 flex flex-wrap text-l border-double p-5">
 
 <Card title="Pikachu - 0" image="https://placehold.co/400x400" />
 <Card title="02" image=""/>
@@ -11,9 +11,10 @@ export const Root = () => {return <div className="space-x-2 space-y-2 flex flex-
 <Card title="06" image=""/>
 
 
+
 </div>};
 
 const Card = (props:{title:string,image:string}) => {
-  return <div className="bg-white w-40 h-40 text-up flex flex-wrap rounded-md px-4 pt-4">  {props.title}
+  return <div className="bg-white w-40 h-50 text-up flex flex-wrap rounded-md px-4 pt-4 border-solid border-4 border-gray-500">  {props.title}
 <img src={props.image}/> </div>
 }
