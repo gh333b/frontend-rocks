4a97dbbfb7b705e10ba3df0669431505be5ac006
gh333b <151088778+gh333b@users.noreply.github.com>
2026-01-15 15:15:34 +0000

    integrazione API

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,20 +1,54 @@
-//import { useState, useEffect } from "react";
-//import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import { PokeAPI } from "./api";
 
-export const Root = () => {return <div className="space-x-2 space-y-2 flex flex-wrap text-l border-double p-5">
+type Pokemon = {
+  id: number;
+  name: string;
+  image: string;
+};
 
-<Card title="Pikachu - 0" image="https://placehold.co/400x400" />
-<Card title="02" image=""/>
-<Card title="03" image=""/>
-<Card title="04" image=""/>
-<Card title="05" image=""/>
-<Card title="06" image=""/>
+async function fetchData(){
+  const response = await PokeAPI.listPokemons()
+  return response.results.map((item, idx) => ({
+          id: idx,
+          name: item.name,
+          image: "https://placehold.co/400x400", 
+        }))
+} 
+const Card = ({ image, title }: { image?: string; title?: string }) => {
+  return (
+    <div className="bg-white w-40 flex flex-col rounded-md shadow-md overflow-hidden border border-gray-300">
+      {image && (
+        <img
+          src={image || "https://placehold.co/400x400"}
+          alt={title || ""}
+          className="w-40 h-40 object-cover"
+        />
+      )}
+      {title && <p className="p-2 text-center font-bold">{title}</p>}
+    </div>
+  );
+};
 
+export const Root = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
+  useEffect(() => {
+    PokeAPI.listPokemons().then((response) => {
+      setPokemons(response.results.map((item, idx) => ({
+          id: idx,
+          name: item.name,
+          image: "https://placehold.co/400x400", 
+        }))
+      );
+    });
+  }, []);
 
-</div>};
-
-const Card = (props:{title:string,image:string}) => {
-  return <div className="bg-white w-40 h-50 text-up flex flex-wrap rounded-md px-4 pt-4 border-solid border-4 border-gray-500">  {props.title}
-<img src={props.image}/> </div>
-}
+  return (
+    <div className="p-5 flex flex-wrap gap-4 justify-center">
+      {pokemons.map((pokemon) => (
+        <Card key={pokemon.id} title={pokemon.name} image={pokemon.image} />
+      ))}
+    </div>
+  );
+};
